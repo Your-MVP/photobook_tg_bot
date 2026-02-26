0fb2716b75164d38453ba272de570afeb9fe5323
DiamondeX <DiamondeX@users.noreply.github.com>
2026-02-26 21:22:07 +0100

    Update pull_and_rebuild.sh

--- pull_and_rebuild.sh
+++ pull_and_rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Pull latest changes from develop branch
-git pull origin develop || { echo "Error: Git pull failed"; exit 1; }
+# Pull latest changes
+git pull origin || { echo "Error: Git pull failed"; exit 1; }
 
 # Rebuild and restart Docker Compose
 docker compose down && docker compose up -d --build || { echo "Error: Docker operation failed"; exit 1; }
